56ef6b03164cdb3ef9ef340f319ba9e361369e5e
Airton Araujo <airton.araujo.s18@gmail.com>
2021-02-18 20:40:13 -0300

    consumindo a rota para criaçao da task

--- frontend/src/components/TaskForm/index.js
+++ frontend/src/components/TaskForm/index.js
@@ -1,9 +1,51 @@
-import './styles.css'
+import { useState } from 'react';
+import api from '../../service/api';
+import './styles.css';
+
 
 function TaskForm() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleChangeTitle = (event) => {
+
+        setTitle(event.target.value)
+    }
+
+    const handleChangeDescription = (event) => {
+        setDescription(event.target.value)
+    }
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        try {
+            await api.post('/', {
+                title,
+                description
+            });
+        } catch (error) {
+            console.log('Ocorreu um erro ao adicionar um regisrto' + error);
+        }
+    }
+
+
     return (
-        <form action="" className="form">
-            <input type="text" className="task-input"></input>
+        <form onSubmit={handleSubmit} className="form ">
+            <input type="text"
+                className="task-input"
+                placeholder="Add title"
+                required
+                value={title}
+                onChange={handleChangeTitle}></input>
+
+            <input type="text"
+                className="task-input"
+                placeholder="Add description" required
+                value={description}
+                onChange={handleChangeDescription}></input>
+
             <div className="buttons">
                 <button type="submit" className="btn add-task-btn">Adicionar</button>
                 <button className="btn clear-btn">Limpar</button>
